5f989c01a74479adb3fce9444f08bb82fc7741c9
CSS-Scripter <thomleenman@gmail.com>
2022-12-04 12:28:57 +0100

    removed comments

--- 2022/elixir/2/main.exs
+++ 2022/elixir/2/main.exs
@@ -6,15 +6,15 @@ defmodule Day2 do
 
   def one() do
     scoremap = %{
-      "A X" => 4, #(1 + 3),
-      "A Y" => 8, #(2 + 6),
-      "A Z" => 3, #(3 + 0),
-      "B X" => 1, #(1 + 0),
-      "B Y" => 5, #(2 + 3),
-      "B Z" => 9, #(3 + 6),
-      "C X" => 7, #(1 + 6),
-      "C Y" => 2, #(2 + 0),
-      "C Z" => 6  #(3 + 3)
+      "A X" => 4,
+      "A Y" => 8,
+      "A Z" => 3,
+      "B X" => 1,
+      "B Y" => 5,
+      "B Z" => 9,
+      "C X" => 7,
+      "C Y" => 2,
+      "C Z" => 6
     }
     getInput("input.txt")
     |> Enum.map(fn x -> scoremap[x] end)
@@ -24,15 +24,15 @@ defmodule Day2 do
 
   def two() do
     scoremap = %{
-      "A X" => 3, #(3 + 0),
-      "A Y" => 4, #(1 + 3),
-      "A Z" => 8, #(2 + 6),
-      "B X" => 1, #(1 + 0),
-      "B Y" => 5, #(2 + 3),
-      "B Z" => 9, #(3 + 6),
-      "C X" => 2, #(2 + 0),
-      "C Y" => 6, #(3 + 3),
-      "C Z" => 7  #(1 + 6)
+      "A X" => 3,
+      "A Y" => 4,
+      "A Z" => 8,
+      "B X" => 1,
+      "B Y" => 5,
+      "B Z" => 9,
+      "C X" => 2,
+      "C Y" => 6,
+      "C Z" => 7
     }
     getInput("input.txt")
     |> Enum.map(fn x -> scoremap[x] end)
